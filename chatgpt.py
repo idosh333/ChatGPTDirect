fb5416dc76ff318a02471dcfe608aeadbbc4071c
Ido Shayovitz <ishayovitz@magicsoftware.com>
2023-03-02 09:07:12 +0200

    Added ctrl+enter support and a readme

--- chatgpt.py
+++ chatgpt.py
@@ -1,66 +1,77 @@
+import html
 import streamlit as st
 import openai
 import os
+import keyboard
 
 from datetime import datetime
 from dotenv import load_dotenv
 
 
-# Initialize the session state
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
-
 # Get api key from .env file
-
 load_dotenv()
-
 openai.api_key = os.environ.get('API_KEY')
+
 model_engine = "text-davinci-003"  # set the default language model
 
 st.set_page_config(page_title="GPT Direct",
                    page_icon=":speech_balloon:", layout="wide")
 
 
-def generate_response(prompt, model_engine, temperature=0.5, max_tokens=60):
-    if "session_chat_history" not in st.session_state:
-        st.session_state.session_chat_history = []
-    response = openai.Completion.create(
-        engine=model_engine,
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=max_tokens,
-    )
-    message = response.choices[0].text.strip()
-    time = datetime.now().strftime("%H:%M:%S")
-    st.session_state.session_chat_history.append((prompt, message, time))
-    return message
+def generate_response(prompt, model_engine, temperature=0.5, max_tokens=1024):
+    with st.spinner(text="Thinking..."):
+        prompt_history = get_prompt_history()
+        full_prompt = (prompt_history + ' answer this: ' + prompt).strip()
+        response = openai.Completion.create(
+            engine=model_engine,
+            prompt=full_prompt,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            stop=None,
+        )
+        message = response.choices[0].text.strip()
+        add_to_chat_history(prompt, message)
+
+
+def get_prompt_history():
+    if len(st.session_state.chat_history) < 1:
+        return ""
+    history = ""
+    for tup in st.session_state.chat_history:
+        history += f'My prompt was: {tup[0].strip()}. Your response was: {tup[1].strip()}.'
+    return history.strip()
 
 
 def add_to_chat_history(prompt, response):
-    time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    st.session_state.chat_history.append((time, prompt, response))
+    time = datetime.now().strftime("%H:%M:%S")
+    st.session_state.chat_history.append((prompt, response, time))
 
 
-def chat_display(chat_history):
+def display_chat():
     # Display the chat history
-    for chat_entry in chat_history:
+    for chat_entry in st.session_state.chat_history:
+
+        prompt = html.escape(chat_entry[0])
+        response = html.escape(chat_entry[1])
+        time = html.escape(chat_entry[2])
+
         st.write(
-            f'<div class="message"><b>Prompt:</b> {chat_entry[1]}<span style="float: right">{datetime.now().strftime("%H:%M:%S")}</span></div>', unsafe_allow_html=True)
+            f'<div class="message"><b>Prompt:</b> {prompt}<span style="float: right">{time}</span></div>', unsafe_allow_html=True)
         st.write(
-            f'<div class="message"><b>Response:</b> {chat_entry[2]}<span style="float: right">{datetime.now().strftime("%H:%M:%S")}</span></div>', unsafe_allow_html=True)
+            f'<div class="message"><b>Response:</b> {response}<span style="float: right">{time}</span></div>', unsafe_allow_html=True)
+
+
+def on_prompt_change(prompt):
+    if len(prompt) > 3 and "Control" in prompt and "Enter" in prompt:
+        generate_response(prompt, model_engine)
 
 
 def main():
     st.title("Chat with GPT API Directly")
 
     # Initialize the session state
-    if "key_pressed" not in st.session_state:
-        st.session_state.key_pressed = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
-
-    # Initialize the variables
-    message = ""
 
     # Styling
     st.markdown(
@@ -90,44 +101,22 @@ def main():
         format_func=lambda x: x.upper()
     )
 
-    # Create a column for the send button
-    if st.session_state.key_pressed == "Enter":
-        st.session_state.key_pressed = None
-        st.text_input("", "", key="hidden")
-
     # Create the input prompt field and send button
     input_container = st.container()
-    with input_container:
-        prompt = st.text_input("Type your prompt here", key="prompt")
-        st.write("")
-
-    # Send the prompt when the send button is clicked
     send_button = st.button("Send", key="send_button")
 
-    col1, col2 = st.columns([4, 1])
-    if len(prompt) > 3:
-        with st.spinner(text="Thinking..."):
-            message = generate_response(prompt, model_engine)
-            add_to_chat_history(prompt, message)
-    else:
-        col2.write(" ")
+    with input_container:
+        prompt = st.text_area("Type your prompt here",
+                              key="prompt", height=50, max_chars=20000)
 
-    if st.session_state.key_pressed is not None:
-        js = ""
-        if st.session_state.key_pressed == "Down":
-            js = "window.scrollTo(0, document.body.scrollHeight);"
-            html = f'< img src ="data: image/png'
-            add_to_chat_history(prompt, message)
-            
-    chat_display(st.session_state.chat_history)
+    if send_button and len(prompt) > 3:
+        generate_response(prompt, model_engine)
+        input_container.empty()
 
-    # Listen for key events
-    key_event = st.session_state.get("key_event")
-    if key_event is not None:
-        if key_event["type"] == "KEY_DOWN" and key_event["key"] == "Enter":
-            st.session_state.key_pressed = "Enter"
+    if keyboard.is_pressed("ctrl") and keyboard.is_pressed("enter") and len(prompt) > 3:
+        generate_response(prompt, model_engine)
 
-        st.experimental_set_query_params(key_event=None)
+    display_chat()
 
 
 if __name__ == "__main__":
